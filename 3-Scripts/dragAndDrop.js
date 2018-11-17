ca2d9e6a9cc5564ef2dbdb76121d8fc8966332a1
Woich <minewoichbr@gmail.com>
2018-11-17 18:57:06 -0200

    Teste 2

--- 3-Scripts/dragAndDrop.js
+++ 3-Scripts/dragAndDrop.js
@@ -2,8 +2,16 @@ window.onload = function () {
    alert("carregado...");
 }
 
+var dragSrcEl = null;
+
 function handleDragStart(e) {
-  this.style.opacity = '0.4';  // this / e.target is the source node.
+  // Target (this) element is the source node.
+  this.style.opacity = '0.4';
+
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
 }
 
 function handleDragOver(e) {
@@ -31,4 +39,39 @@ var cols = document.querySelectorAll('#columns .column');
   col.addEventListener('dragenter', handleDragEnter, false);
   col.addEventListener('dragover', handleDragOver, false);
   col.addEventListener('dragleave', handleDragLeave, false);
+});
+
+function handleDrop(e) {
+  // this/e.target is current target element.
+
+  if (e.stopPropagation) {
+    e.stopPropagation(); // Stops some browsers from redirecting.
+  }
+
+  // Don't do anything if dropping the same column we're dragging.
+  if (dragSrcEl != this) {
+    // Set the source column's HTML to the HTML of the column we dropped on.
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+
+  return false;
+}
+
+function handleDragEnd(e) {
+  // this/e.target is the source node.
+
+  [].forEach.call(cols, function (col) {
+    col.classList.remove('over');
+  });
+}
+
+var cols = document.querySelectorAll('#columns .column');
+[].forEach.call(cols, function(col) {
+  col.addEventListener('dragstart', handleDragStart, false);
+  col.addEventListener('dragenter', handleDragEnter, false)
+  col.addEventListener('dragover', handleDragOver, false);
+  col.addEventListener('dragleave', handleDragLeave, false);
+  col.addEventListener('drop', handleDrop, false);
+  col.addEventListener('dragend', handleDragEnd, false);
 });
