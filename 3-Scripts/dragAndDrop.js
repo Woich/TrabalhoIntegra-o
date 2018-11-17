9d43c75272d431939a97e11fd7f2e0677f5cb793
Woich <minewoichbr@gmail.com>
2018-11-17 17:14:58 -0200

    Adição do JQUERY

--- 3-Scripts/dragAndDrop.js
+++ 3-Scripts/dragAndDrop.js
@@ -1,8 +1,12 @@
+window.onload = function () {
+   alert("carregado...");
+}
+
 function handleDragStart(e) {
   this.style.opacity = '0.4';  // this / e.target is the source node.
 }
 
 var cols = document.querySelectorAll('#columns .column');
 [].forEach.call(cols, function(col) {
-  col.addEventListener('dragstart', handleDragStart, false);
+    col.addEventListener('dragstart', handleDragStart, false);
 });
